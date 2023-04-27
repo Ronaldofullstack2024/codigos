361085626fb04c1c9e4d0a0b3ffc3d707eb396e9
Talentos C08100O <UG_TALENTOS_C08100O@asav.brm>
2023-04-27 08:56:17 -0300

    subindo exercicio de operadores

---
+++ index.js
@@ -0,0 +1,95 @@
+console.log("hello")
+// comentario
+//prompt("Digite o seu nome")
+
+
+//const nome = prompt("digite seu nome")
+//const idade = prompt("digite sua idade")
+//const endereco = prompt("digite seu endereco")
+//console.log(nome)
+//console.log(idade)
+//console.log(endereco)
+//-------------------------------------------------------------------------------------------------------------------------
+let anonascimento = 1997
+
+anonascimento = 2023 - anonascimento
+
+console.log(anonascimento)
+//.................................................................
+
+// const nome = prompt("digite seu nome")
+// const idade = prompt("digite sua idade")
+// console.log ("Olá!", "Meu nome é ", nome, "e eu tenho", idade, "anos")
+
+//.....................................................................................................................
+
+//const primeironome = prompt("digite seu primeiro nome?")
+//const sobrenome = prompt("digite seu sobrenome?")
+//const idade = prompt("digite sua idade?")
+//const estudante = prompt("vc é estudante? (sim)/nao)")
+//console.log("primeironome","sobrenome","idade","estudante")
+
+//...................................................................................................................
+
+//const got = "Game of thrones"
+//const temporadas = 8
+
+//console.log(typeof got)
+//console.log(typeof temporadas)
+
+//.................................................................
+
+//let a = 10
+//let b = 10
+
+//console.log(b)
+
+//b = 5
+//console.log(a, b)
+
+//.....................................................................
+
+let nome
+let idade
+
+console.log(typeof nome);
+console.log(typeof idade);
+// essas variáveis não tem valor, porque os valores não foram declarados.
+
+//nome = String(prompt("informe seu nome:"))
+//idade = Number(prompt("informe a sua idade:"))
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+
+//console.log(" seu nome é:" + nome)
+//console.log("Olá"+ nome +" sua idade é" + idade +"anos")
+
+const pergunta = prompt ("voce comeu hoje? (sim/nao")
+
+console.log ("primeira pergunta:", pergunta);
+
+let a = Number(prompt("digite um numero"))
+let b = Number(prompt("digite outro numero"))
+
+let c = b
+b = a
+a = c
+
+console.log("valor do primeiro numero",a);
+console.log("valor do segundo numero", b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
